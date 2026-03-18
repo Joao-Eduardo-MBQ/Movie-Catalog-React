a1060f89dad482d6d51ee11e67156b892a81efb9
Joao Eduardo <joao.eduardo.quadro@gmail.com>
2026-03-17 22:11:15 -0300

    Primeiro commit do projeto Movie Catalog React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/Card.jsx'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Card />
+    </div>
+  )
+}
+
+export default App
